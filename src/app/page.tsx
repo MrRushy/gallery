77f7a4bd3e929ebc3ca2763d32a7a676805e67c8
Rushy <rushilshanmugam@gmail.com>
2026-05-04 12:28:18 -0400

    fixef pag.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className= "flex min-h-screen flex-col items-center justify-center bg-gradient-to-b fromm-[#2e026d] to-[#15162c] text-white">
-      Hello World! Gallery in Progress...
+     <main className="flex min-h-screen
+      flex-col items-center justify-center
+       bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      Hello... Gallery in progress
     </main>
-  );
+  )
 }
